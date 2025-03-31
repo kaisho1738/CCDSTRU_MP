d8198021dd54848bb410556552607b8cce7a850b
huwawmarqss <marquezchino06@gmail.com>
2025-03-31 19:07:59 +0800

    Update functions_V0_5.c

--- functions_V0_5.c
+++ functions_V0_5.c
@@ -3,14 +3,15 @@
 #define TOTAL_POSITIONS (SIZE * SIZE)
 
 /*
-Last Modified: Kim | 5:15 | 3/31/25
+Last Modified: Chino | 7:05PM | 3/31/25
 Version:
     +1 Working Prototype
     +0.1 Finished a function
     +0.01 Small Changes/Debugging
 ---------------------------------------
 Changes:
-
+- Changed size of array for Uno, Tres, Dos to TOTAL_POSITIONS
+- Changed i < SIZE to i < TOTAL_POSITIONS of for loop of DeclareNull
 --------------------------------------
 Code Notes [Bugs or other stuff]:
 
@@ -23,14 +24,14 @@ typedef struct{
 } coordinate;
 
 coordinate P[TOTAL_POSITIONS];
-coordinate T[SIZE];
-coordinate Uno[SIZE];
-coordinate Tres[SIZE];
-coordinate Dos[SIZE];
+coordinate T[TOTAL_POSITIONS];
+coordinate Uno[TOTAL_POSITIONS];
+coordinate Tres[TOTAL_POSITIONS];
+coordinate Dos[TOTAL_POSITIONS];
 coordinate F[TOTAL_POSITIONS]; //All coordinates/ordered pairs not Uno and Tres
 
 void DeclareNull(coordinate Uno[], coordinate Tres[]){
-    for(int i = 0; i < SIZE; i++){
+    for(int i = 0; i < TOTAL_POSITIONS; i++){
         Tres[i].x = 0;
         Tres[i].y = 0;
         Uno[i].x = 0;
